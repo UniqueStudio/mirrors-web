ca656c8c806b9876d2028678f135d636dff6146e
Mirror <mirror@mirrors.hustunique.com>
2014-01-06 13:40:18 +0800

    Bring back next sync column

--- assets/js/application.js
+++ assets/js/application.js
@@ -100,9 +100,9 @@ var initialize = function() {
 		var name = $(this).attr("id");
 		$(this).append($("<td id=\"" + name + "-upstream\">"),
 					   $("<td id=\"" + name + "-size\">"),
-					   $("<td id=\"" + name + "-status\">"),
+					   //$("<td id=\"" + name + "-status\">"),
 					   $("<td id=\"" + name + "-lastsync\">"),
-					   //$("<td id=\"" + name + "-nextsync\">"),
+					   $("<td id=\"" + name + "-nextsync\">"),
 					   $("<td>").html("<i id=\"" + name + "-userreq\" class=\"icon-refresh\" title=\"Request to synchronize now\" onclick=\"request_user_update('" + name + "');\"></i>"),
 					   $("<td>").html("<i id=\"" + name + "-logs\" class=\"icon-file\" onclick=\"show_log('" + name + "');\"></i>"));
 	});
